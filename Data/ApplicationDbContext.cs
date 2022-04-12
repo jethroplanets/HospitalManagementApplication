10a0a419894b6bde8896b9c74d73878b5d5c1ab0
jethroplanets <71425074+jethroplanets@users.noreply.github.com>
2022-04-11 18:03:07 -0700

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HospitalMgt.Models;
+
+namespace HospitalMgt.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HospitalMgt.Models.Dispense> Dispense { get; set; }
+        public DbSet<HospitalMgt.Models.Doctor> Doctor { get; set; }
+        public DbSet<HospitalMgt.Models.Patient> Patient { get; set; }
+        public DbSet<HospitalMgt.Models.Medicine> Medicine { get; set; }
+        public DbSet<HospitalMgt.Models.AppUser> AspNetUsers { get; set; }
+    }
+}
---
+++ Data/Migrations/20220408164947_migration1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalMgt.Data.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicine",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicine", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dispense",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DispensedBy = table.Column<Guid>(nullable: false),
+                    DispensedTo = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dispense", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dispense_Doctor_DispensedBy",
+                        column: x => x.DispensedBy,
+                        principalTable: "Doctor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Dispense_Patient_DispensedTo",
+                        column: x => x.DispensedTo,
+                        principalTable: "Patient",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dispense_DispensedBy",
+                table: "Dispense",
+                column: "DispensedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dispense_DispensedTo",
+                table: "Dispense",
+                column: "DispensedTo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dispense");
+
+            migrationBuilder.DropTable(
+                name: "Medicine");
+
+            migrationBuilder.DropTable(
+                name: "Doctor");
+
+            migrationBuilder.DropTable(
+                name: "Patient");
+        }
+    }
+}
